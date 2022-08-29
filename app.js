f76c0dd311d793d4eedd4d071687facdcbd2f962
Muhammad Ali <muhammad.alee.api@gmail.com>
2022-08-30 01:09:34 +0500

    second commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+/*
 // Answer 1
 var firstName = prompt('Enter First Name');
 var lastName = prompt('Enter Last Name');
@@ -64,22 +64,56 @@ var userInput = prompt('Enter Anything');
 document.write('<h1>User Input: '+ userInput +'</h1>');
 userInput = userInput[0].toUpperCase() + userInput.slice(1);
 document.write('<h1>Title Case: '+ userInput +'</h1>');
+
 // Answer 12
 var num = 35.36;
 document.write('<h1>Number: '+ num +'</h1>');
 num = num.toString();
 num = num.replace('.','');
 document.write('<h1>Result: '+ num +'</h1>');
+
 // Answer 13
-var userInput = prompt('Enter Username');
-var error = 0;
-for(var i = 0; i < userInput.length; i++){
-    if( userInput[i] == '!' || userInput[i] == ',' || userInput[i] == '.' || userInput[i] == '@' || userInput[i] == '[' || userInput[i] == ']'){
-        error = 1;
+do {
+    var error = 0;
+    var userInput = prompt('Enter Username');
+    for(var i = 0; i < userInput.length; i++){
+        if( userInput[i] == '!' || userInput[i] == ',' || userInput[i] == '.' || userInput[i] == '@' || userInput[i] == '[' || userInput[i] == ']'){
+            error = 1;
+        }
+    }
+    if(error){
+        alert('Please Enter a Valid Username');
+    }else{
+        alert('Username is valid');
+    }
+    
+} while (error);
+
+// Answer 14
+var itemsArr = ['cake','apple pie','cookie','chips','patties']
+var userInput = prompt('Welcome to Expertizo Bakery. What do you want to order?');
+var itemFound = 0;
+var index = 0;
+userInput = userInput.toLowerCase();
+for (var i = 0; i < itemsArr.length; i++) {
+    if(itemsArr[i] === userInput){
+        itemFound = 1; index = i;
     }
 }
-if(error){
-    alert('Please Enter a Valid Username');
+if (itemFound) {
+    document.write('<h1>' + userInput + ' is available at index ' + index + ' in our bakery</h1>');
 }else{
-    alert('Username is valid');
+    document.write('<h1>We are Sorry. ' + userInput + ' is not available in our bakery</h1>');
 }
+// Answer 15
+var userPassword = prompt('Enter Password');
+
+// Answer 16
+var university = 'University of Karachi';
+university = university.split('');
+for (let i = 0; i < university.length; i++) {
+    document.write(university[i] +"<br>");
+}
+*/
+// Answer 17
+var userInput = prompt('Enter any string');
